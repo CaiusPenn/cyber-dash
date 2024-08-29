e283eef704eabe6453c86aaa7436d4b791073116
CaiusPenn <caiuspenn@gmail.com>
2024-08-29 19:56:39 +1000

    attempt 1 at login page

---
+++ auth.ts
@@ -0,0 +1,48 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
+ 
+async function getUser(username: string): Promise<User | undefined> {
+  try {
+    // Querying by username instead of email
+    const user = await sql<User>`SELECT * FROM users WHERE username=${username}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
+ 
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        // Update schema to use username and password
+        const parsedCredentials = z
+          .object({ username: z.string(), password: z.string().min(5) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { username, password } = parsedCredentials.data;
+          const user = await getUser(username);
+
+          if (user) {
+            const passwordsMatch = await bcrypt.compare(password, user.password);
+
+            if (passwordsMatch) {
+              return user;
+            }
+          }
+        }
+
+        console.log('Invalid credentials');
+        return null;
+      },
+    }),
+  ],
+});
