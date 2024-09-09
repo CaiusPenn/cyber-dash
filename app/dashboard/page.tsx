f9488b8d193ba68ed35bf2eb6261a6f435cf84fb
CaiusPenn <caiuspenn@gmail.com>
2024-09-09 14:06:25 +1000

    login working

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import React from "react";
 import Overview from "./overview"; // Corrected import
+import { signOut } from '@/auth';
+
 const Manager = () => {
   return (
     <Grid
@@ -18,6 +20,19 @@ const Manager = () => {
       <GridItem rowSpan={1} colSpan={13} bg="papayawhip" />
       <GridItem rowSpan={1} colSpan={13} bg="papayawhip" />
       <GridItem rowSpan={1} colSpan={13} bg="papayawhip" />
+
+      <form
+        action={async () => {
+          'use server';
+          await signOut();
+        }}
+        >
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
+
     </Grid>
   );
 };
