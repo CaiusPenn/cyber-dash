126f0de0b689a30dfba38ce2d1d75120fe05e735
CaiusPenn <caiuspenn@gmail.com>
2024-09-09 14:16:50 +1000

    added login functionality with default admin user

--- app/dashboard/admin/overview.tsx
+++ app/dashboard/admin/overview.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HStack, Select, Stack, Text } from "@chakra-ui/react";
+import { signOut } from '@/auth';
 
 const Overview = () => {
   return (
@@ -17,8 +18,20 @@ const Overview = () => {
         <Select placeholder="Date Range" size="lg" maxWidth={300} />
         <Select placeholder="Category" size="lg" maxWidth={300} />
       </HStack>
+      <form
+        action={async () => {
+          'use server';
+          await signOut();
+        }}
+        >
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
     </>
   );
 };
 
 export default Overview;
+
