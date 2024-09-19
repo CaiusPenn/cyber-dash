ab10546f1b62ac1e31d3b68f5d1c3b2b304e5559
Ivana <21btrcc017@jainuniversity.ac.in>
2024-09-20 09:31:50 +1000

    check for manager_id

--- auth.ts
+++ auth.ts
@@ -5,12 +5,14 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
-import { createSession } from './app/lib/session';
+import { createSession } from '@/app/lib/session';
+
+
 
 async function getUser(email: string): Promise<User | undefined> {
     try {
       const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-      console.log(user.rows[0]);
+
       return user.rows[0];
     } catch (error) {
       console.error('Failed to fetch user:', error);
@@ -24,7 +26,7 @@ async function getUser(email: string): Promise<User | undefined> {
       Credentials({
         async authorize(credentials) {
           const parsedCredentials = z
-            .object({ email: z.string().email(), password: z.string().min(5) })
+            .object({ email: z.string().email(), password: z.string().min(6) })
             .safeParse(credentials);
           if (parsedCredentials.success) {
             const { email, password } = parsedCredentials.data;
@@ -32,10 +34,19 @@ async function getUser(email: string): Promise<User | undefined> {
             if (!user) return null;
             const passwordsMatch = await bcrypt.compare(password, user.password);
 
-            if (passwordsMatch) {
-              //await createSession(user.name,user.job_type);
+            if (passwordsMatch){
+              const manager_id = await sql<Number>`SELECT manager_id from departments WHERE manager_id=${user.id}`;
+              const man_id = manager_id.rows[0];
+              console.log(man_id);
+              const user_id = <string><unknown>user.id;
+              if (man_id){
+                await createSession(user_id,user.username,"manager");
+              }
+              else{
+                await createSession(user_id,user.username,"user");
+              }
               return user as any;
-            }
+            } 
           }
           return null;
         },
