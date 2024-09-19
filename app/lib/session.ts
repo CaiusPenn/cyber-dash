7110e6c8508c53f5a871334518e17aee7923496f
Ivana <21btrcc017@jainuniversity.ac.in>
2024-09-20 09:32:46 +1000

    set role cookie and id

--- app/lib/session.ts
+++ app/lib/session.ts
@@ -1,11 +1,20 @@
 import 'server-only'
 import { cookies } from 'next/headers'
  
-export async function createSession(user: string,job_type:string) {
+export async function createSession(id:string,user: string,job_type:string) {
   const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  const user_id = id;
   const name = user;
   const role = job_type;
- 
+  
+  cookies().set('id', user_id, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: 'lax',
+    path: '/',
+  })
+
   cookies().set('name', name, {
     httpOnly: true,
     secure: true,
